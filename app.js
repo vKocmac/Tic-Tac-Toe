e0cec0b9693aa7a1332188b000f7ae61319f522e
vKocmac <vkocmac@gmail.com>
2022-12-08 22:07:32 +0200

    fix JS logic and bugs with currentPlayer variable

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-
+const gridItem = document.querySelectorAll(".grid-item");
+let currentPlayer;
 const Gameboard = (()=>{
   const gameboard = ["","","","","","","","",""];
   return {gameboard};
@@ -14,45 +15,50 @@ const SelectMarker = (()=>{
   const xButton = document.querySelector(".x-mark");
   const oButton = document.querySelector(".o-mark");
   xButton.addEventListener("click", ()=>{
-    console.log("iam in");
      Game.player1.marker = "x";
      Game.player2.marker = "o";
     console.log(Game.player1,Game.player2);
-
+    currentPlayer = 1;
+    Render();
 
   })
   oButton.addEventListener("click", ()=>{
     Game.player1.marker = "o";
     Game.player2.marker = "x";
     console.log(Game.player1,Game.player2);
+    currentPlayer = 1;
+    Render();
   })
 })();
 
-const Render = (()=>{
-  const gridItem = document.querySelectorAll(".grid-item");
+const Render = ()=>{
+
   gridItem.forEach((item, i) => {
     item.addEventListener("click", ()=>{
-      if(i%2 === 0){
-        Gameboard.gameboard[i] =Game.player1.marker;
-          item.innerHTML = Game.player1.marker;
-          console.log(Gameboard.gameboard);
-      }else{
-        Gameboard.gameboard[i] = Game.player2.marker;
-          item.innerHTML = Game.player2.marker;
-          console.log(Gameboard.gameboard);
+      if(item.innerHTML === ""){
+        if (currentPlayer === 1){
+          console.log("render1");
+          Gameboard.gameboard[i] =Game.player1.marker;
+            item.innerHTML = Game.player1.marker;
+            console.log(Gameboard.gameboard);
+            currentPlayer=2;
+        }else{
+          console.log("render2");
+          Gameboard.gameboard[i] =Game.player2.marker;
+            item.innerHTML = Game.player2.marker;
+            console.log(Gameboard.gameboard);
+            currentPlayer = 1;
+        }
       }
     });
   });
-})();
+};
+
 
 const Game = (()=>{
+
   const player1 = Player("Player 1", "");
   const player2 = Player("Player 2", "");
   return {player1, player2}
-SelectMarker();
-  // const player1 = Player("Player 1", "x");
-  // const player2 = Player("Player 2", "o");
-  //  return {player1, player2};
-
 
 })();
